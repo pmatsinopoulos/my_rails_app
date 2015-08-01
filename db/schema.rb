c4a769e0a7a0788275e3fbfd9fdfdf81326318fd
Richard Cox <webstartyours@gmail.com>
2015-08-01 14:22:24 +0100

    Column added to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722215834) do
+ActiveRecord::Schema.define(version: 20150801131818) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150722215834) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
 end
