14a84753099e40c9cfba98ff04810a1d6ae984d7
Richard Cox <webstartyours@gmail.com>
2015-09-05 19:50:52 +0100

    Created comments model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
   has_many :orders
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902205906) do
+ActiveRecord::Schema.define(version: 20150905123310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.integer  "rating"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["product_id"], name: "index_comments_on_product_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -59,4 +71,6 @@ ActiveRecord::Schema.define(version: 20150902205906) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
 end
