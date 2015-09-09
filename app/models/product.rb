7a4962719be585ee8e0c0dc280b966294bc81d8a
Richard Cox <webstartyours@gmail.com>
2015-09-09 06:05:21 +0100

    Implemented raty plugin

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
   has_many :orders
   has_many :comments
+  
+  def average_rating
+    comments.average(:rating).to_f
+  end
 end
