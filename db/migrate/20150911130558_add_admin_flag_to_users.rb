9e70a4c8827296f6752f8fcd717f1abd1dd3cae2
Richard Cox <webstartyours@gmail.com>
2015-09-13 22:41:31 +0100

    Updated migration file

--- db/migrate/20150911130558_add_admin_flag_to_users.rb
+++ db/migrate/20150911130558_add_admin_flag_to_users.rb
@@ -2,4 +2,7 @@ class AddAdminFlagToUsers < ActiveRecord::Migration
   def change
     add_column :users, :admin, :boolean
   end
+  
+  def set_user_admin
+    update User.find(2) :admin => true
 end
