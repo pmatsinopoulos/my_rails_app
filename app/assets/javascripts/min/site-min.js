1310db915bad65ae55291392fd616f9bf3558a15
Richard Cox <webstartyours@gmail.com>
2015-10-30 23:41:33 +0000

    AJAX functionality added for adding new comments

--- app/assets/javascripts/min/site-min.js
+++ app/assets/javascripts/min/site-min.js
@@ -1,1 +1,1 @@
-$(document).on("ready page:load",function(){$(".rating").raty({path:"/assets/images",scoreName:"comment[rating]"}),$(".rated").raty({path:"/assets/images",readOnly:!0,score:function(){return $(this).attr("data-score")}}),$(".img-zoom").elevateZoom()});
+function addRating(){$(".rating").raty({path:"/assets/images",scoreName:"comment[rating]"}),$(".rated").raty({path:"/assets/images",readOnly:!0,score:function(){return $(this).attr("data-score")}})}$(document).on("ready page:load",function(){addRating(),$(".img-zoom").elevateZoom()});
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,5 @@
-$(document).on('ready page:load', function(){
-    // Add rating sytem
+function addRating(){
+  // Add rating sytem
     $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets/images', 
       readOnly: true,
@@ -7,6 +7,9 @@ $(document).on('ready page:load', function(){
         return $(this).attr('data-score');
       }
     });
+}
+$(document).on('ready page:load', function(){
+    addRating();
     
     // Add image zoom
     $('.img-zoom').elevateZoom();
