b7f69ca7d999f6bd60f11380a4c793834897b480
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2015-11-02 09:09:57 +0200

    I have fixed the product show page cache. I now use a better version for the #cache_key and I do not longer need any cache call around the comments fragment. The #cache_key takes into account the number of comments itself, so, if anything changes (new comment or deleted comment) the comments displayed on the product will be refreshed/rebuilt and not taken from cache.

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,13 @@ class Product < ActiveRecord::Base
   def average_rating
     comments.average(:rating).to_f
   end
-  
+
+  # We make sure that the cache_key contains the default cache_key value (from 'super')
+  # plus the number of comments for the particular product. So, if any comment is added
+  # or deleted, we make sure that the product show page will be invalidated from cache
+  # and will be rebuilt again.
+  #
   def cache_key
-  "#{id}-#{updated.to_s(:number)}"
+  "#{super}-#{comments.count}"
   end
 end
