6b5267c32d7e60f239d33ad58b2a99eaae7c9823
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2015-11-02 09:08:24 +0200

    Caching for the Products index page. We are using a helper method that builds a custom key based on number of products and the maximum updated_at date.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,10 @@
 module ProductsHelper
+  # This is the way we build the cache key for the index page.
+  # We invalidate the cache whenever a product is added or deleted (Product.count),
+  # or whenever a product is updated (by including in the key the maximum updated_at date)
+  def cache_key_for_products
+    count          = Product.count
+    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "products/all-#{count}-#{max_updated_at}"
+  end
 end
